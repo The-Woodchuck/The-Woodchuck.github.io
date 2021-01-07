6f02fa345c91371e199867076526d0ce07ea56df
The Woodchuck <77108558+The-Woodchuck@users.noreply.github.com>
2021-01-07 17:10:12 +0200

    Auto learn todo

--- Main.js
+++ Main.js
@@ -4,7 +4,11 @@
 /*TODO: A more complex auto-learn that lets you give detailed instructions on what level you want each skill and in what order.
 					ie "Train Productivity to 50" -> "Train Meditation to 50" -> "Train Productivity to 100" -> "Train Strength to 100" etc
 */
-
+/*
+TODO
+Make auto-learn use exp/day and remaining to work out what to level next.
+Add a filter to turn on and off skills to learn.
+*/
 
 //Game speed and calculations
 
